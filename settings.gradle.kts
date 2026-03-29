91b5155c9691c14cc8424804a15574b917bce053
Masashi Katsumata <wf9a5m75@gmail.com>
2026-03-29 12:25:17 +0900

    port from the android-sdk repository

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/mapconductor/android-marker-clustering")
+            credentials {
+                username = System.getenv("GPR_USER")
+                password = System.getenv("GPR_TOKEN")
+            }
+            content { includeGroup("com.mapconductor") }
+        }
+    }
+}
+
+rootProject.name = "mapconductor-marker-clustering"
