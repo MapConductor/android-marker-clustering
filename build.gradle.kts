6b4abee4fc73fdf304b3c4f26483b450bb5d1dcd
Masashi Katsumata <wf9a5m75@gmail.com>
2026-06-07 07:40:47 +0900

    fix: build error

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,15 +57,6 @@ android {
     }
 }
 
-kotlin {
-    compilerOptions {
-        jvmTarget.set(
-            org.jetbrains.kotlin.gradle.dsl.JvmTarget.fromTarget(
-                project.property("jvmTarget").toString(),
-            ),
-        )
-    }
-}
 
 dependencies {
     if (findProject(":android-sdk-core") != null) {
