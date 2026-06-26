dad42581bce29da8bc9084f4ed02e6d247da9402
Masashi Katsumata <wf9a5m75@gmail.com>
2026-06-27 08:56:28 +0900

    chore: bump library versions to 1.0.1/1.1.8 and fix publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,9 +107,7 @@ val javadocJar by tasks.registering(Jar::class) {
 publishing {
     publications {
         create<MavenPublication>("release") {
-            project.afterEvaluate {
-                from(components["release"])
-            }
+            from(components["release"])
 
             groupId = libraryGroupId
             artifactId = libraryArtifactId
